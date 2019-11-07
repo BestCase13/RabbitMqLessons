af58a9490be56980729e4dbc8e0a2ce9f3a9d4ae
BestCase13 <panchyshyn.andrew@gmail.com>
2019-11-07 19:45:25 +0200

    Regular commit

---
+++ EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace EmitLogDirect
+{
+	class EmitLogDirect
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() {HostName = "localhost"};
+			while (true)
+			{
+				using (var connection = factory.CreateConnection())
+				using (var channel = connection.CreateModel())
+				{
+					channel.ExchangeDeclare(exchange: "direct_logs",
+						type: "direct");
+
+					var rnd = new Random();
+					var severity = rnd.Next(1, 2);
+
+					var message = severity == 1
+						? $"Error Message {DateTime.Now.ToLocalTime()}"
+						: $"Info Message {DateTime.Now.ToLocalTime()}";
+
+					var body = Encoding.UTF8.GetBytes(message);
+
+					channel.BasicPublish(exchange: "direct_logs",
+						routingKey: severity.ToString(),
+						basicProperties: null,
+						body: body);
+
+					Console.WriteLine($" [x] Sent {message} with {severity}");
+
+					Thread.Sleep(2000);
+				}
+			}
+		}
+	}
+}
---
+++ ReceiveLogDirect/ReceiveLogDirect.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace ReceiveLogDirect
+{
+	class ReceiveLogDirect
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() {HostName = "localhost"};
+
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+				var queueName = channel.QueueDeclare().QueueName;
+			}
+		}
+
+		private void DisplayError(string message)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine(message);
+			Console.ResetColor();
+		}
+		private void DisplayInfo(string message)
+		{
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.WriteLine(message);
+			Console.ResetColor();
+		}
+	}
+}
