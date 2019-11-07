e93e512c2fdb85c1f019c2a5778d609318cef6dc
BestCase13 <panchyshyn.andrew@gmail.com>
2019-11-07 17:57:05 +0200

    Completed lesson 3

---
+++ EmitLogger/EmitLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace EmitLogger
+{
+	class EmitLogger
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() {HostName = "localhost"};
+			while (true)
+			{
+				using (var connection = factory.CreateConnection())
+				using (var channel = connection.CreateModel())
+				{
+					channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+					var message = GetMessage();
+
+					var body = Encoding.UTF8.GetBytes(message);
+					channel.BasicPublish(exchange:"logs", routingKey:"", basicProperties:null, body:body);
+
+					Console.WriteLine($"[x] Sent {message}");
+
+					Thread.Sleep(3000);
+				}
+			}
+		}
+
+		private static string GetMessage()
+		{
+			return $"La La Land {DateTime.Now.ToLocalTime()}";
+		}
+	}
+}
