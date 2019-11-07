c90314e9e97c042c3d2ecc2f2c593d9650f745c8
BestCase13 <panchyshyn.andrew@gmail.com>
2019-11-07 13:03:26 +0200

    Add project files.

---
+++ Lesson1/Receiver/Receiver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Receiver
+{
+	class Receiver
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() {HostName = "localhost"};
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(queue: "hello", durable:false, exclusive:false, autoDelete:false, arguments:null);
+				string message = "Hello world";
+				var body = Encoding.UTF8.GetBytes(message);
+
+			}
+		}
+	}
+}
